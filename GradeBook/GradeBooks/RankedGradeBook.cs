d4f58893bdf77522e8d8c96386431f43503d20cf
Donovan Guerrero <num.2ron@hotmail.com>
2018-08-23 14:10:44 -0600

    Step 2
    Added GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +10,42 @@ namespace GradeBook.GradeBooks
     public RankedGradeBook(string name) : base(name)
     {
       Type = GradeBookType.Ranked;
+    }
+
+    public override char GetLetterGrade(double averageGrade)
+    {
+      if (Students.Count < 5)
+      {
+        throw new InvalidOperationException();
+      }
+
+      var threshold = (int)Math.Ceiling(Students.Count * .20);
+      var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+      if (grades[threshold - 1] <= averageGrade)
+      {
+        return 'A';
+      }
+
+      else if (grades[(threshold * 2) - 1] <= averageGrade)
+      {
+        return 'B';
+      }
+
+      else if (grades[(threshold * 3) - 1] <= averageGrade)
+      {
+        return 'C';
+      }
+
+      else if (grades[(threshold * 4) - 1] <= averageGrade)
+      {
+        return 'D';
+      }
+
+      else
+      {
+        return 'F';
+      }
     }
   }
 }
